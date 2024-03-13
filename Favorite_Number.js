7a90a02d2e756b71917ee7151458b2e9ae6055b6
Hamza Nasir <161011944+hamzanasir123@users.noreply.github.com>
2024-03-13 14:20:36 +0500

    first commit

---
+++ Favorite_Number.js
@@ -0,0 +1,4 @@
+"use strict";
+const FavoriteNumber = 5;
+const message = 'Here is your favorite number ' + FavoriteNumber;
+console.log(message);
---
+++ Favorite_Number.ts
@@ -0,0 +1,7 @@
+const FavoriteNumber :number = 5 ;
+
+const message :string = 'Here is your favorite number ' + FavoriteNumber ;
+
+console.log(message);
+
+
